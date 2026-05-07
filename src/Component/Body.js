bd2af5d10236d0197c79a4e81ee9acf5c3541133
Rohit Singh <rohitsingh91322@gmail.com>
2026-05-07 19:13:50 +0530

    Added useParams and github api feature

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -1,33 +1,38 @@
 import { useEffect, useState } from "react";
+import useFetch from "../useFetch";
 
 function Body(){
-    // body me card ko display karna hai therefore jo bhi card display karna hai unki information fetch karna padega
+
+// ye wala part useFetch mein likh do , this is the custom hook
+const {generateProfile,numberofProfile,setnumberofProfile,Profile}= useFetch();
+
+//     // body me card ko display karna hai therefore jo bhi card display karna hai unki information fetch karna padega
   
-  const [Profile,setProfile] = useState([]);
-  const[numberofProfile,setnumberofProfile] = useState("");
+//   const [Profile,setProfile] = useState([]);
+//   const[numberofProfile,setnumberofProfile] = useState("");
 
 
 
-// ye user ke data ko la raha hai fetch wala
- async function generateProfile(count){
-    const ran = Math.floor(1+Math.random()*1000);
+// // ye user ke data ko la raha hai fetch wala
+//  async function generateProfile(count){
+//     const ran = Math.floor(1+Math.random()*1000);
   
-    const response =await fetch(`https://api.github.com/users?since=${ran}&per_page= ${count}`); // yaha pe hamne js ka variable daal dia `` isse lagana important hai
-    const data =await response.json();
+//     const response =await fetch(`https://api.github.com/users?since=${ran}&per_page= ${count}`); // yaha pe hamne js ka variable daal dia `` isse lagana important hai
+//     const data =await response.json();
   
-    // respose wale data ko json file  mein convert karega
+//     // respose wale data ko json file  mein convert karega
     
 
-    setProfile(data);
-    // initailly toh profile khali hogi par jab data aayega toh usse setProfile mein daal denge
-  }
+//     setProfile(data);
+//     // initailly toh profile khali hogi par jab data aayega toh usse setProfile mein daal denge
+//   }
 
-  useEffect(()=>{
-   generateProfile(10);  //  means initially 10 profile hi dekhegi
-  },[]);
+//   useEffect(()=>{
+//    generateProfile(10);  //  means initially 10 profile hi dekhegi
+//   },[]);
 
-  // empty array rakha hai yaha taki ye function ek hi baar call ho
-  // kyunki mujhe ek hi baar chahiye , ek hi baar profile frtch kar sake
+//   // empty array rakha hai yaha taki ye function ek hi baar call ho
+//   // kyunki mujhe ek hi baar chahiye , ek hi baar profile frtch kar sake
   
   return (
     <div className="but">
